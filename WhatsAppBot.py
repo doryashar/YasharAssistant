bd1ad063bfa0800309def89948684322631da8c5
doryashar <dor.yashar@gmail.com>
2023-05-21 15:41:22 +0300

    .

--- WhatsAppBot.py
+++ WhatsAppBot.py
@@ -51,6 +51,7 @@ async def handle_data(data):
             logging.info(f"New Message; sender:{mobile} name:{name} type:{message_type}")
             if message_type == "text":
                 message = messenger.get_message(data)
+                messenger.mark_as_read(messenger.get_message_id(data))
                 mid = data.get('entry')[0]['id']
                 if mid in known_ids:
                     logging.error('id already used')
